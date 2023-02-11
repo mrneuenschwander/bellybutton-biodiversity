51dec44cf2cfdacf151f56851b3faadcbc6ec02e
mrneuenschwander <116296092+mrneuenschwander@users.noreply.github.com>
2023-02-11 16:49:06 -0700

    finish D2 and 3
    - add bubble chart
    - add gauge readout
    - commented out a bunch of console logs
    - many thanks to the Plotly documentation

--- Challenge/charts.js
+++ Challenge/charts.js
@@ -58,7 +58,7 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   // Deliverable 1: 2. Use d3.json to load the samples.json file 
   d3.json("samples.json").then((data) => {
-    console.log(data);
+    // console.log(data);
 
     // Deliverable 1: 3. Create a variable that holds the samples array. 
     var samples = data.samples;
@@ -71,15 +71,15 @@ function buildCharts(sample) {
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
     var metadata = data.metadata;
     var volunteerInfo = metadata.filter(sampleObj => sampleObj.id == sample);
-    console.log(volunteerInfo);
+    // console.log(volunteerInfo);
 
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
     var firstSample = selectedSample[0];
-    console.log(firstSample);
+    // console.log(firstSample);
 
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
     var firstHost = volunteerInfo[0];
-    console.log(firstHost);
+    // console.log(firstHost);
 
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = firstSample.otu_ids;
@@ -96,7 +96,7 @@ function buildCharts(sample) {
     // so the otu_ids with the most bacteria are last. 
     var topTenOTU = otu_ids.slice(0,10)
     var yticks = topTenOTU.reverse().map(i => "OTU " + i)
-    console.log(yticks)
+    // console.log(yticks)
 
     var topTenValues = sample_values.slice(0,10)
     var xticks = topTenValues.reverse()
@@ -118,19 +118,59 @@ function buildCharts(sample) {
 
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
     // Deliverable 2: 1. Create the trace for the bubble chart.
     var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      name: "Bacteria",
+      mode: "markers",
+      marker:{
+        size: sample_values,
+        color: otu_ids,
+        colorScale: "YlGnBu"
+      }
+    }];
 
-    }]
     // Deliverable 2: 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacterial Cultures per Sample",
+      xaxis: {
+        title: "OTU ID"
+      }
+    };
 
     // Deliverable 2: 3. Use Plotly to plot the data with the layout.
-    
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
     // Deliverable 3: 4. Create the trace for the gauge chart.
-    
+    var gaugeData = [{
+      domain:{x: [0,10], y:[0,10]},
+      value: wfreq,
+      title: "Belly Button Washing Frequency<br>Scrubs per Week",
+      type: "indicator",
+      mode:"gauge+number",
+      gauge:{
+        axis:{range:[null, 10]},
+        steps:[
+          {range: [0, 2], color: "red"},
+          {range: [2, 4], color: "orange"},
+          {range: [4, 6], color: "yellow"},
+          {range: [6, 8], color: "green"},
+          {range: [8, 10], color: "darkgreen"}
+        ],
+        bar:{color: "black"}
+      }
+    }];
+
     // Deliverable 3: 5. Create the layout for the gauge chart.
+    var gaugeLayout = [{
+      width: 500,
+      margin: {t: 25, r: 25, l: 25, b:25}
+    }];
 
     // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
-
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout)
   });
-}
+};
